430e868d634e36ee574fb5e67ff6f07835bf51f8
刘老师666 <527161772@qq.com>
2021-09-23 15:30:16 +0800

    构建mapper接口

---
+++ springboot_demo3_mybatis/src/main/java/com/jt/mapper/DemoUserMapper.java
@@ -0,0 +1,17 @@
+package com.jt.mapper;
+
+import com.jt.pojo.DemoUser;
+
+import java.util.List;
+
+/**
+ * 说明:
+ *      1.根据面向接口开发的思想需要定义一个Mapper接口
+ *      2.在接口中可以写接口方法, 谁用谁去实现!!!
+ */
+public interface DemoUserMapper {
+
+    //1.查询所有的表数据
+    public List<DemoUser> findAll();
+
+}
