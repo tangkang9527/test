2707e242cdf48bf406fe9719e78b4252cda14979
刘老师666 <527161772@qq.com>
2021-09-23 14:42:41 +0800

    添加jar包

--- springboot_demo2/src/main/java/com/jt/pojo/DemoUser.java
+++ springboot_demo2/src/main/java/com/jt/pojo/DemoUser.java
@@ -1,5 +1,9 @@
 package com.jt.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
@@ -13,10 +17,29 @@ import java.io.Serializable;
  *      5.一般实体对象需要实现序列化接口(规则)
  *          原因: 数据可能跨平台(跨服务器)传输,必须序列化
  */
+@Data  //动态生成get/set/toString/equals等方法
+@Accessors(chain = true) //开启链式加载 重写set方法
+@NoArgsConstructor  //无参构造
+@AllArgsConstructor //有参构造
 public class DemoUser implements Serializable {
     private Integer id;
     private String name;
     private Integer age;
     private String sex;
+
+    //this 运行期有效 代表当前对象
+    /*public DemoUser setId(Integer id){
+        this.id = id;
+        return this;
+    }*/
+
+    //方法测试
+    public void add(){
+        DemoUser user = new DemoUser();
+        user.setId(100)
+            .setName("aaaa")
+            .setAge(18)
+            .setSex("女");
+    }
 
 }
