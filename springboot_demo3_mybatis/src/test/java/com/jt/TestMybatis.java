7cd2bf3eea2e47c625a1f9ccdb674e55f7910f23
刘老师666 <527161772@qq.com>
2021-09-23 16:18:09 +0800

    完成MP入门案例

--- springboot_demo3_mybatis/src/test/java/com/jt/TestMybatis.java
+++ springboot_demo3_mybatis/src/test/java/com/jt/TestMybatis.java
@@ -1,11 +1,17 @@
 package com.jt;
 
+import com.jt.mapper.DemoUserMapper;
+import com.jt.pojo.DemoUser;
+import org.apache.catalina.User;
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class TestMybatis {
 
@@ -15,6 +21,7 @@ public class TestMybatis {
      *      1.动态生成SqlSessionFactory
      *
      */
+    @Test
     public void demo1() throws IOException {
         //指定配置文件地址
         String resource = "mybatis/mybatis-config.xml";
@@ -23,5 +30,12 @@ public class TestMybatis {
         //动态生成SqlSessionFactory
         SqlSessionFactory sqlSessionFactory =
                                 new SqlSessionFactoryBuilder().build(inputStream);
+        //获取SqlSession  类比 数据库链接
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        //获取Mapper接口
+        DemoUserMapper demoUserMapper = sqlSession.getMapper(DemoUserMapper.class);
+        //获取数据
+        List<DemoUser> userList = demoUserMapper.findAll();
+        System.out.println(userList);
     }
 }
