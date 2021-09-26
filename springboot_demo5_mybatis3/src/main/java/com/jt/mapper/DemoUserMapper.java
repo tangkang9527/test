6c77177dce4e225fa4ef80b71eddc3abb07dd32f
刘老师666 <527161772@qq.com>
2021-09-26 10:36:52 +0800

    创建项目3

--- springboot_demo5_mybatis3/src/main/java/com/jt/mapper/DemoUserMapper.java
+++ springboot_demo5_mybatis3/src/main/java/com/jt/mapper/DemoUserMapper.java
@@ -8,12 +8,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * 说明:
- *      1.根据面向接口开发的思想需要定义一个Mapper接口
- *      2.在接口中可以写接口方法, 谁用谁去实现!!!
- *      3.Mybatis中的实现类以xml文件的形式存在
- */
 public interface DemoUserMapper {
 
     List<DemoUser> findAll();
