3aa25d8ae79fcfa9759eccd6badb97a13264e533
刘老师666 <527161772@qq.com>
2021-09-26 11:36:26 +0800

    Sql标签简化配置

--- springboot_demo5_mybatis3/src/main/java/com/jt/pojo/DemoUser.java
+++ springboot_demo5_mybatis3/src/main/java/com/jt/pojo/DemoUser.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
+import org.apache.ibatis.type.Alias;
 
 import java.io.Serializable;
 
@@ -11,6 +12,7 @@ import java.io.Serializable;
 @Accessors(chain = true)
 @NoArgsConstructor
 @AllArgsConstructor
+@Alias("DemoUser")
 public class DemoUser implements Serializable {
 
     private Integer id;
