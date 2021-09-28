284d2ecb2b2d778a5e8d1cf3a76b53516a489bfd
刘老师666 <527161772@qq.com>
2021-09-28 15:01:28 +0800

    spring整合Mybatis(终)

--- springboot_ssm/src/main/java/com/jt/SpringbootSsmApplication.java
+++ springboot_ssm/src/main/java/com/jt/SpringbootSsmApplication.java
@@ -1,13 +1,15 @@
 package com.jt;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//根据包扫描路径 扫描全部的mapper接口文件
+@MapperScan("com.jt.mapper")
 public class SpringbootSsmApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootSsmApplication.class, args);
     }
-
 }
--- springboot_ssm/src/main/java/com/jt/mapper/UserMapper.java
+++ springboot_ssm/src/main/java/com/jt/mapper/UserMapper.java
@@ -1,9 +1,13 @@
 package com.jt.mapper;
 
 import com.jt.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
-
+//将该接口交给Spring容器管理
+//@Mapper
 public interface UserMapper {
     //查询demo_user表中的所有数据
     List<User> findAll();
