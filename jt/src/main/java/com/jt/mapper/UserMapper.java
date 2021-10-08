cbc22e7e233ade5c581ab142ae691fba80077531
刘老师666 <527161772@qq.com>
2021-10-08 17:46:05 +0800

    完成用户登录(一)

--- jt/src/main/java/com/jt/mapper/UserMapper.java
+++ jt/src/main/java/com/jt/mapper/UserMapper.java
@@ -1,10 +1,13 @@
 package com.jt.mapper;
 
 import com.jt.pojo.User;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface UserMapper {
 
     List<User> findAll();
+    @Select("select * from user where username=#{username} and password=#{password}")
+    User findUserByUP(User user);
 }
--- jt/src/main/java/com/jt/service/UserServiceImpl.java
+++ jt/src/main/java/com/jt/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.jt.mapper.UserMapper;
 import com.jt.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
 import java.util.List;
 
@@ -19,8 +20,21 @@ public class UserServiceImpl implements UserService{
         return userMapper.findAll();
     }
 
+    //根据u/p查询数据库
     @Override
     public String findUserByUP(User user) {
-        return null;
+        //1.将密码加密
+        byte[] bytes = user.getPassword().getBytes();
+        String md5Pass = DigestUtils.md5DigestAsHex(bytes);
+        user.setPassword(md5Pass);
+        //2.根据用户名和密文查询数据库
+        User userDB = userMapper.findUserByUP(user);
+        //3.判断userDB是否有值
+        if(userDB == null){
+            //查询没有数据.返回null
+            return null;
+        }
+        String token = "秘钥";
+        return token;
     }
 }
