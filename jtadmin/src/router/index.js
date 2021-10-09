0bb41dbe70783bc5c2144645edbb1abd4acad1e4
刘老师666 <527161772@qq.com>
2021-10-09 12:05:26 +0800

    路由导航守卫

--- jtadmin/src/router/index.js
+++ jtadmin/src/router/index.js
@@ -12,10 +12,40 @@ const routes = [
   {path: '/home', component: Home}
 ]
 
-//路由导航守卫!!!!!!!
 
+//路由对象的定义
 const router = new VueRouter({
   routes
 })
 
+/**
+ * 路由导航守卫实现
+ * 说明: beforeEach用户的每次请求在访问目标网址之前都会被拦截.
+ * 参数:
+ *     1.to    用户请求的网址对象
+ *     2.from  用户请求的来源
+ *     3.next  请求的放行
+ *  需求:
+ *     1.如果用户访问登录页面
+ *     2.校验是否含有token数据
+ *        有token  放行
+ *        没有token  跳转到登录页面
+ */
+router.beforeEach((to,from,next) => {
+    if(to.path === '/login'){
+      //直接跳转并且结束代码
+      return next()
+    }
+    //2.判断是否有token
+    let token = window.sessionStorage.getItem("token")
+    if(token !== null && token !==''){ //更加安全
+      //有值,跳转指定页面
+      return next()
+    }
+    //3.如果数据为空则跳转到登录页面
+    next('/login')
+})
+
+
+//路由对象的使用
 export default router
