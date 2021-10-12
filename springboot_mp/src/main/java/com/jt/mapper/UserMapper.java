c012177f66534b1e2f147b29f6af71c9b19c7d4a
刘老师666 <527161772@qq.com>
2021-10-12 14:12:41 +0800

    MP入门案例-整合mybatisplus

--- springboot_mp/src/main/java/com/jt/mapper/UserMapper.java
+++ springboot_mp/src/main/java/com/jt/mapper/UserMapper.java
@@ -1,12 +1,12 @@
 package com.jt.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.jt.pojo.User;
 import org.apache.ibatis.annotations.*;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-public interface UserMapper {
+public interface UserMapper extends BaseMapper<User> {
     List<User> findAll();
-
 }
