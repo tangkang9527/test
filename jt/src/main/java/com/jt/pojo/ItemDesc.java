cdde2cecf9ab298f5e41f900caa34025c9b2de86
刘老师666 <527161772@qq.com>
2021-10-14 17:05:49 +0800

    商品详情实现

--- jt/src/main/java/com/jt/pojo/ItemDesc.java
+++ jt/src/main/java/com/jt/pojo/ItemDesc.java
@@ -1,7 +1,12 @@
 package com.jt.pojo;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import lombok.experimental.Accessors;
+
+import java.lang.reflect.Type;
 
 /**
  * @author 刘昱江
@@ -9,8 +14,10 @@ import lombok.experimental.Accessors;
  */
 @Data
 @Accessors(chain = true)
+@TableName("item_desc")
 public class ItemDesc extends BasePojo{
-
+    //由于item.id=itemDesc.id 所以ID不能主键自增
+    @TableId
     private Integer id;
     private String itemDesc;
 
