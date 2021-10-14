d557be7b855e80ae3a21b1343d7b980722c32e82
刘老师666 <527161772@qq.com>
2021-10-14 17:38:51 +0800

    实现商品详情入库操作

--- jt/src/main/java/com/jt/service/ItemServiceImpl.java
+++ jt/src/main/java/com/jt/service/ItemServiceImpl.java
@@ -3,12 +3,15 @@ package com.jt.service;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.jt.mapper.ItemDescMapper;
 import com.jt.mapper.ItemMapper;
 import com.jt.pojo.Item;
+import com.jt.pojo.ItemDesc;
 import com.jt.vo.ItemVO;
 import com.jt.vo.PageResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import java.util.List;
@@ -18,6 +21,8 @@ public class ItemServiceImpl implements ItemService{
 
     @Autowired
     private ItemMapper itemMapper;
+    @Autowired
+    private ItemDescMapper itemDescMapper;
 
 
     /**
@@ -47,9 +52,16 @@ public class ItemServiceImpl implements ItemService{
     }
 
     @Override
+    @Transactional
     public void saveItem(ItemVO itemVO) {
-        Item item = itemVO.getItem();
+        Item item = itemVO.getItem(); //id=null
         item.setStatus(true);
+        //要求item入库之后,动态返回Id!!!!
+        //MP原则: 入库之后动态回显数据!!
         itemMapper.insert(item);
+        //实现itemDesc对象入库
+        ItemDesc itemDesc = itemVO.getItemDesc();
+        itemDesc.setId(item.getId());
+        itemDescMapper.insert(itemDesc);
     }
 }
